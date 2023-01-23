36eae7fe36e662d95b53daca1f068f82fa6a65e4
Tom Draper <tomjdraper1@gmail.com>
2023-01-23 17:36:04 +0000

    Switch to requests

--- src/scrape.py
+++ src/scrape.py
@@ -1,38 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
+import requests
+from bs4 import BeautifulSoup
 
 def request_url(url: str):
-    options = Options()
-    options.headless = True  # hide GUI
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("start-maximized")
-
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=options)
-    driver.get(url)
-
-    return driver
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
 
 
 def scrape_img(url: str) -> list[str]:
-    driver = request_url(url)
-    elements = driver.find_elements(By.TAG_NAME, 'img')
-    links = [{'src': element.get_property('src'),
-              'alt': element.get_property('alt'),
-              'height': element.get_property('height'),
-              'width': element.get_property('width')}
-             for element in elements]
+    soup = request_url(url)
+    links = [{'src': a['src'], 'alt': a['alt']} for a in soup.find_all('img')]
     return links
 
 
 def scrape_link(url: str) -> list[str]:
-    driver = request_url(url)
-    elements = driver.find_elements(By.TAG_NAME, 'a')
-    links = [{'text': element.text,
-              'href': element.get_property('href')}
-             for element in elements]
+    soup = request_url(url)
+    links = [{'href': a['href']} for a in soup.find_all('a')]
     return links
